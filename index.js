204df45fa810c8f4ebe6734923ff8ae15aa1b7d1
prasantdev <prasantsahoorebel@gmail.com>
2022-09-22 00:09:49 +0530

    fix path issue

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.set('view engine', 'ejs');
-
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   // res.send('Hello World!');
-  res.render('index');
+  res.render(path.join(__dirname, 'views', 'index.ejs'));
 });
 
 app.listen(PORT, () => {
